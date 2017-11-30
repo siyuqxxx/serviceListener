725dc70fe59330e310200630f0d226f5dbd3e8ca
siyuqxxx <523527519@qq.com>
2017-11-30 20:28:21 +0800

    # `增加` 创建类 LisServerBean 为 LisServer 提供一些功能

---
+++ src/main/java/com/zt/serviceListener/model/LisServerBean.java
@@ -0,0 +1,29 @@
+package com.zt.serviceListener.model;
+
+import com.zt.serviceListener.pojo.LisServer;
+
+import java.net.InetSocketAddress;
+
+public class LisServerBean extends LisServer{
+    public LisServerBean() {
+    }
+
+    public LisServerBean(LisServer lisServer) {
+        super.setIp(lisServer.getIp());
+        super.setPort(lisServer.getPort());
+    }
+
+    /**
+     * 判断一组，ip 和 port 是否可以被解析；不考虑不能连接的情况
+     * @return true - 不能被解析 | false - 可以正常解析
+     */
+    public boolean isUnresolved()
+    {
+        return getSocketAddress().isUnresolved();
+    }
+
+    public InetSocketAddress getSocketAddress()
+    {
+        return new InetSocketAddress(super.getIp(), super.getPort());
+    }
+}

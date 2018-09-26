e56f9e73f907f74e365013e7eb47d652685ec9a2
qiansy <qiansy@beijingzhangtu.com>
2018-09-26 17:52:01 +0800

    # `修复` 链接超时导致卡死问题

--- src/main/java/com/zt/serviceListener/util/UrlDetectUtil.java
+++ src/main/java/com/zt/serviceListener/util/UrlDetectUtil.java
@@ -8,6 +8,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLConnection;
 import java.util.Objects;
 import java.util.Scanner;
 
@@ -24,20 +25,30 @@ public class UrlDetectUtil {
             return "";
         }
 
-        StringBuilder b = new StringBuilder();
-        try (InputStream inputStream = url.openStream()) {
-            Scanner s = new Scanner(inputStream);
-            while (s.hasNext()) {
-                if (b.length() < Constants.MAX_URL_RESPONSE_LENGTH) {
-                    b.append(s.next());
-                } else {
-                    LOG.warn("url response is over max length.");
-                    LOG.info(String.format("Top %d response msg: %s...", Constants.MAX_URL_RESPONSE_LENGTH, b.toString()));
-                    break;
+        try {
+            URLConnection urlConnection = url.openConnection();
+            urlConnection.setConnectTimeout(30000);
+            urlConnection.setReadTimeout(30000);
+            urlConnection.connect();
+
+            StringBuilder b = new StringBuilder();
+            try (InputStream inputStream = urlConnection.getInputStream()) {
+                Scanner s = new Scanner(inputStream);
+                while (s.hasNext()) {
+                    if (b.length() < Constants.MAX_URL_RESPONSE_LENGTH) {
+                        b.append(s.next());
+                    } else {
+                        LOG.warn("url response is over max length.");
+                        LOG.info(String.format("Top %d response msg: %s...", Constants.MAX_URL_RESPONSE_LENGTH, b.toString()));
+                        break;
+                    }
                 }
+                LOG.debug(b.toString());
+                return b.toString();
             }
-            LOG.debug(b.toString());
-            return b.toString();
+        } catch (IOException e) {
+            LOG.error("url cannot be connected.");
+            return "";
         }
     }
 
